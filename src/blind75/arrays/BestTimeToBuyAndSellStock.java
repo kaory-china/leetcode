e671bb14d2d5ccd82a554cd2733cc625b9dba27e
kaory-china <kaory_china@hotmail.com>
2023-01-30 23:22:33 -0300

    second attempt to best time to buy and sell stock, but its by brute force and times out for very large input arrays

--- src/blind75/arrays/BestTimeToBuyAndSellStock.java
+++ src/blind75/arrays/BestTimeToBuyAndSellStock.java
@@ -3,36 +3,24 @@ package blind75.arrays;
 public class BestTimeToBuyAndSellStock {
 
   public static void main(String[] args) {
-    System.out.println(maxProfit(new int[]{7,1,5,3,6,4}));
-    System.out.println(maxProfit(new int[]{7,6,4,3,1}));
+    System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+    System.out.println(maxProfit(new int[]{7, 6, 4, 3, 1}));
+    System.out.println(maxProfit(new int[]{2, 4, 1}));
   }
 
   public static int maxProfit(int[] prices) {
-    int minValue = prices[0];
-    int minIndex = 0;
+    int maxProfit = 0;
 
-    for (int i = 0; i < prices.length; i++) {
-      if (prices[i] < minValue) {
-        minValue = prices[i];
-        minIndex = i;
+    for (int i = 0; i < prices.length - 1; i++) {
+      for (int j = i + 1; j < prices.length; j++) {
+        if (prices[j] > prices[i]) {
+          if (prices[j] - prices[i] > maxProfit) {
+            maxProfit = prices[j] - prices[i];
+          }
+        }
       }
     }
 
-    if (minIndex == prices.length-1) return 0;
-
-    int maxValue = prices[minIndex + 1];
-
-    for (int j = minIndex + 1;  j < prices.length; j++) {
-      if (prices[j] > maxValue) {
-        maxValue = prices[j];
-      }
-    }
-
-    if (maxValue - minValue < 0) {
-      return 0;
-    }
-
-    return maxValue - minValue;
+    return maxProfit;
   }
-
 }
