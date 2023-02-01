f38105c4e91fc0d753b22fcdbd625f791e9f21c1
kaory-china <kaory_china@hotmail.com>
2023-01-31 22:25:41 -0300

    attempt 3 to best time to buy and sell stock, sliding window technique

--- src/blind75/arrays/BestTimeToBuyAndSellStock.java
+++ src/blind75/arrays/BestTimeToBuyAndSellStock.java
@@ -9,18 +9,17 @@ public class BestTimeToBuyAndSellStock {
   }
 
   public static int maxProfit(int[] prices) {
+    int cheapestPrice = prices[0];
     int maxProfit = 0;
 
     for (int i = 0; i < prices.length - 1; i++) {
-      for (int j = i + 1; j < prices.length; j++) {
-        if (prices[j] > prices[i]) {
-          if (prices[j] - prices[i] > maxProfit) {
-            maxProfit = prices[j] - prices[i];
-          }
-        }
+      if (prices[i] < cheapestPrice) {
+        cheapestPrice = prices[i];
+      }
+      if (prices[i + 1] - cheapestPrice > maxProfit) {
+        maxProfit = prices[i + 1] - cheapestPrice;
       }
     }
-
     return maxProfit;
   }
 }
